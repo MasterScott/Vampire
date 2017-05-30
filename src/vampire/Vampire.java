4fbbc098849536b9c2c3d744348ea6a53bddb6bd
Cryptotoad <lsd@love.com>
2017-05-30 01:08:42 -0700

    etc

--- src/vampire/Vampire.java
+++ src/vampire/Vampire.java
@@ -5,8 +5,14 @@
  */
 package vampire;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URL;
 import java.net.URLClassLoader;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import vampire.ui.mainUI;
 
 /**
@@ -21,9 +27,10 @@ public class Vampire {
     private static URL jarURL;
     private static clientConfig conf;
     private static mainUI window;
+    private static ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     
-    public static void main(String[] args) {
-        conf = new clientConfig();
+    public static void main(String[] args) throws Exception {
+        conf = clientConfig.loadJson();
         window = new mainUI();
         window.setVisible(true);
     }
@@ -35,6 +42,18 @@ public class Vampire {
         Class mainClass = jarLoader.loadClass(conf.getMainClass());
         Object mainInstance = mainClass.newInstance();
         mainClass.getMethod("main").invoke(mainInstance);
+    }
+    
+    public static String handleCommand(String command) throws Exception {
+        String scriptPath = conf.getScriptPath() + command + ".js";
+        File f = new File(scriptPath);
+        if(!f.exists()) {
+            return "Error! Invalid command!";
+        }
+        engine.eval(new FileReader(scriptPath));
+        Invocable run = (Invocable) engine;
+        Object result = run.invokeFunction("command");
+        return (String) result;
     }
     
     
